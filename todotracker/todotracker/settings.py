3c2f38d2f0f8eac08d4275ef1d12e16f40321243
Charan Lingamgunta <clingamgunta@forumsys.com>
2018-06-21 00:14:35 -0400

    Reverting uninteded change

--- todotracker/todotracker/settings.py
+++ todotracker/todotracker/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     #}
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ToDoTracker',                      
+        'NAME': 'TodoTracker',                      
         'USER': 'tdt_pg',
         'PASSWORD': 'tdt_pg',
         'HOST': '',
