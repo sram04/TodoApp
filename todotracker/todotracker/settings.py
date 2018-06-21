861d39764d83f82f7377f7e07532d310a19b70a9
Charan Lingamgunta <clingamgunta@forumsys.com>
2018-06-21 00:13:42 -0400

    Remove unneeded migrations

--- todotracker/todotracker/settings.py
+++ todotracker/todotracker/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     #}
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'TodoTracker',                      
+        'NAME': 'ToDoTracker',                      
         'USER': 'tdt_pg',
         'PASSWORD': 'tdt_pg',
         'HOST': '',
